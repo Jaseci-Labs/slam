ff9e585c434cdc2ff856b7f4ad7192eaedc4d967
AshishMahendra <am028275@gmail.com>
2024-01-30 17:08:27 +0000

    updates

--- human_eval/human_eval_app.py
+++ human_eval/human_eval_app.py
@@ -1,193 +1,319 @@
 import streamlit as st
 import json
+import os
 import random
-from itertools import combinations
-import hashlib
 import time
+import hashlib
 
-# Initialize the session state variables
-if "current_combination_index" not in st.session_state:
-    st.session_state.current_combination_index = 0
-    st.session_state.task_completed = False
-    st.session_state.start_time = None
-    st.session_state.models = []
-    st.session_state.combinations = []
-    st.session_state.worker_id = ""
-    st.session_state.button_lock = False  # Initialize the button_lock here
+# File paths
+combinations_file = "data/all_task_completed/distributed_combinations_all_filtered.json"
+unique_codes_file = "data/all_task_completed/unique_codes.json"
+worker_count_file = "data/all_task_completed/worker_count.json"
 
-# Load and parse the JSON data
-with open("inputs.json", "r") as f:
-    data = json.load(f)
+st.set_page_config(layout="wide")
+border_style = """
+<style>
+    .bordered-container {
+        border: 2px solid #e1e4e8;
+        border-radius: 5px;
+        padding: 10px;
+        margin: 10px 0;
+    }
+</style>
+"""
+st.markdown(border_style, unsafe_allow_html=True)
 
 
-# Function to check if the worker ID has already completed the task
-def has_completed(worker_id, file_path="selections.json"):
+# Function to check if the worker has completed the task
+def has_completed():
     try:
-        with open(file_path, "r") as file:
+        with open(
+            f"all_human_data_method_A_all/{st.session_state.worker_id}.json", "r"
+        ) as file:
             all_selections = json.load(file)
-
-        did_work = worker_id in all_selections and all_selections[worker_id][-1].get(
-            "task_completed", False
-        )
-        print(f"Worker ID {worker_id} has completed the task: {did_work}")
-        return did_work
+        return True
     except (FileNotFoundError, json.JSONDecodeError):
         return False
 
 
-# Function to generate a completion code
-def generate_completion_code(worker_id):
-    return hashlib.md5(worker_id.encode()).hexdigest()[:8]
+# Function to retrieve a unique code for the user
+def retrieve_unique_code():
+    with open(unique_codes_file, "r") as file:
+        unique_codes = json.load(file)
+    if not unique_codes:
+        raise Exception("No unique codes available.")
+    user_code = unique_codes.pop(0)
+    with open(unique_codes_file, "w") as file:
+        json.dump(unique_codes, file)
+    return user_code
 
 
-# Function to mark the task as completed in the selections.json
-def mark_task_completed(worker_id, file_path="selections.json"):
+# Function to mark task as completed
+def mark_task_completed(worker_id, completion_code, worker_count):
     try:
-        with open(file_path, "r") as file:
+        with open(
+            f"all_human_data_method_A_all/{st.session_state.worker_id}.json", "r"
+        ) as file:
             all_selections = json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        all_selections = {}
-
-    if worker_id in all_selections:
-        all_selections[worker_id][-1]["task_completed"] = True
-        with open(file_path, "w") as file:
+        all_selections.update(
+            {
+                "task_completed": True,
+                "completion_code": completion_code,
+                "worker_count": worker_count,
+            }
+        )
+        with open(
+            f"all_human_data_method_A_all/{st.session_state.worker_id}.json", "w"
+        ) as file:
             json.dump(all_selections, file, indent=4)
+    except (FileNotFoundError, json.JSONDecodeError):
+        raise Exception(f"file nor found for worker_id {worker_id}")
 
 
-# Function to save the selection to a JSON file
-def save_selection(worker_id, selection, start_time, file_path="selections.json"):
+# Function to update and get worker count
+def update_and_get_worker_count():
     try:
-        with open(file_path, "r") as file:
+        with open(worker_count_file, "r") as file:
+            worker_count = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        worker_count = 0
+
+    worker_count += 1
+    with open(worker_count_file, "w") as file:
+        json.dump(worker_count, file)
+
+    return worker_count
+
+
+def load_combinations():
+    with open(combinations_file, "r") as file:
+        return json.load(file)
+
+
+def are_unique_codes_available():
+    try:
+        with open(unique_codes_file, "r") as file:
+            unique_codes = json.load(file)
+            return len(unique_codes) > 0
+    except (FileNotFoundError, json.JSONDecodeError):
+        return False
+
+
+def main():
+    # Initialization logic here...
+    if "initialized" not in st.session_state:
+        st.session_state.initialized = False
+        st.session_state.current_index = 0
+        st.session_state.start_time = time.time()
+        st.session_state.evaluations = []
+        st.session_state.worker_id = None
+        st.session_state.worker_count = 0
+        st.session_state.combinations = []
+        st.session_state.pair_start_time = time.time()
+        st.session_state.submitted = False
+
+    # Prompt display logic here...
+    prompt = 'Your task is to compare the output of two AI models on the set of criteria specified below. Please choose the model that you perceive to be better, or select "About the same" if there is no clear advantage one way or another. \n\n**The AI is called Myca and she is your personal assistant. You just completed all of your important tasks for today and she is giving you a compliment.**'
+    st.markdown(
+        f"<div class='bordered-container'><p><strong>Instructions</strong>:</p>{prompt}</div>",
+        unsafe_allow_html=True,
+    )
+    # Worker ID input logic
+    if not st.session_state.initialized:
+        worker_id = st.text_input("Enter your worker ID", key="input_worker_id")
+        if worker_id:
+            st.session_state.worker_id = worker_id
+            if has_completed():
+                st.error("You have already completed this task. Thank you.")
+            else:
+                if are_unique_codes_available():
+                    st.session_state.initialized = True
+                    st.session_state.worker_count = update_and_get_worker_count()
+                    st.session_state.combinations = load_combinations().get(
+                        f"worker_{st.session_state.worker_count}", []
+                    )
+                else:
+                    st.error(
+                        "No unique codes are available at this time. Please try again later."
+                    )
+    if st.session_state.initialized and not st.session_state.submitted:
+        if st.session_state.current_index < len(st.session_state.combinations):
+            display_evaluation_interface()
+
+    elif st.session_state.submitted:
+        display_completion_message()
+
+
+def display_evaluation_interface():
+    if "show_warning" not in st.session_state:
+        st.session_state.show_warning = False
+    clarity_key = f"clarity_{st.session_state.current_index}"
+    intelligence_key = f"intelligence_{st.session_state.current_index}"
+    likability_key = f"likability_{st.session_state.current_index}"
+    trustworthy_key = f"trustworthy_{st.session_state.current_index}"
+    # Load the pair only if it hasn't been loaded before
+    if f"pair_{st.session_state.current_index}" not in st.session_state:
+        st.session_state[
+            f"pair_{st.session_state.current_index}"
+        ] = st.session_state.combinations[st.session_state.current_index]
+
+    # Get the current pair from the session state
+    pair = st.session_state[f"pair_{st.session_state.current_index}"]
+    col1, col2, col3 = st.columns([5, 5, 5])
+    # Calculate the current progress and total pairs
+    total_pairs = len(st.session_state.combinations)
+    current_pair_index = (
+        st.session_state.current_index + 1
+    )  # Adding 1 to start count from 1
+    progress_fraction = current_pair_index / total_pairs
+
+    # Display the progress bar and the count
+    st.progress(progress_fraction)
+    st.text(f"Pair {current_pair_index} of {total_pairs}")
+    with col1:
+        st.markdown(
+            f"<div class='bordered-container'><p><strong>Model 1 Response:</strong></p>{pair['response1']}</div>",
+            unsafe_allow_html=True,
+        )
+    with col3:
+        st.markdown(
+            f"<div class='bordered-container'><p><strong>Model 2 Response:</strong></p>{pair['response2']}</div>",
+            unsafe_allow_html=True,
+        )
+    with col2:
+        st.write(
+            "<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>",
+            unsafe_allow_html=True,
+        )
+        st.markdown(
+            """
+                <style>
+                div.row-widget.stRadio > div {
+                    display: flex;
+                    justify-content: center;
+                }
+                /* Center the label text of each radio button */
+                div.row-widget.stRadio > label {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                }
+                /* Optional: Adjust the padding and margin to reduce space */
+                div.row-widget.stRadio {
+                    margin-bottom: 0.25rem;
+                    padding: 0.25rem;
+                }
+                </style>
+                """,
+            unsafe_allow_html=True,
+        )
+        options = ["Select an option", "1 is better", "About the same", "2 is better"]
+        clarity = st.radio(
+            "**Clarity/Conciseness**",
+            options,
+            key=clarity_key,
+        )
+        st.markdown(
+            '<hr style="margin-top: 0.25rem; margin-bottom: 0.25rem;"/>',
+            unsafe_allow_html=True,
+        )
+        intelligence = st.radio(
+            "**Intelligence**",
+            options,
+            key=intelligence_key,
+        )
+        st.markdown(
+            '<hr style="margin-top: 0.25rem; margin-bottom: 0.25rem;"/>',
+            unsafe_allow_html=True,
+        )
+        likability = st.radio(
+            "**Likability**",
+            options,
+            key=likability_key,
+        )
+        st.markdown(
+            '<hr style="margin-top: 0.25rem; margin-bottom: 0.25rem;"/>',
+            unsafe_allow_html=True,
+        )
+        trustworthy = st.radio(
+            "**Trustworthy**",
+            options,
+            key=trustworthy_key,
+        )
+    preference_selected = (
+        clarity != options[0]
+        and likability != options[0]
+        and intelligence != options[0]
+        and trustworthy != options[0]
+    )
+    # Show warning if the user tries to proceed without making a selection
+    if st.session_state.show_warning and not preference_selected:
+        st.warning("Please select an option for all criteria before proceeding.")
+    # 'Next Pair' button logic
+    if st.session_state.current_index < len(st.session_state.combinations) - 1:
+        if st.button("Next Pair") and preference_selected:
+            save_current_evaluation(clarity, intelligence, likability, trustworthy)
+            st.session_state.pair_start_time = time.time()
+            st.session_state.current_index += 1
+            st.session_state.show_warning = False
+            st.experimental_rerun()
+        else:
+            st.session_state.show_warning = True
+    # 'Submit Evaluations' button logic
+    if st.session_state.current_index == len(st.session_state.combinations) - 1:
+        if st.button("Submit Evaluations") and preference_selected:
+            # Save the current evaluation
+            save_current_evaluation(clarity, intelligence, likability, trustworthy)
+            # Submit all evaluations
+            st.session_state.submitted = True
+            st.experimental_rerun()
+        else:
+            st.session_state.show_warning = True
+
+
+def save_current_evaluation(clarity, intelligence, likability, trustworthy):
+    pair = st.session_state[f"pair_{st.session_state.current_index}"]
+    evaluations = {
+        "response1": pair["response1"],
+        "response2": pair["response2"],
+        "clarity": clarity,
+        "intelligence": intelligence,
+        "likability": likability,
+        "trustworthy": trustworthy,
+        "time_spent": round(time.time() - st.session_state.pair_start_time, 2),
+    }
+    all_selections = {}
+    try:
+        with open(
+            f"all_human_data_method_A_all/{st.session_state.worker_id}.json", "r"
+        ) as file:
             all_selections = json.load(file)
     except (FileNotFoundError, json.JSONDecodeError):
-        all_selections = {}
+        all_selections[str(st.session_state.worker_count)] = []
+    all_selections["time_spent"] = round(time.time() - st.session_state.start_time, 2)
+    all_selections[str(st.session_state.worker_count)].append(evaluations)
 
-    if worker_id not in all_selections:
-        all_selections[worker_id] = []
-
-    time_spent = round(time.time() - start_time, 2)
-    selection["time_spent"] = time_spent
-    all_selections[worker_id].append(selection)
-
-    with open(file_path, "w") as file:
+    with open(
+        f"all_human_data_method_A_all/{st.session_state.worker_id}.json", "w"
+    ) as file:
         json.dump(all_selections, file, indent=4)
 
 
-# Function to get a random response from a model
-def get_random_response(model_responses):
-    return random.choice(model_responses)
+def get_radio_index(choice):
+    options = ["1 is better", "About the same", "2 is better"]
+    return options.index(choice) if choice in options else 0
 
 
-# Function to move to the next pair of models
-def next_pair():
-    st.session_state.current_combination_index += 1
-    if st.session_state.current_combination_index == len(st.session_state.combinations):
-        st.session_state.task_completed = True
-
-
-# Title and instructions
-st.title("Human Evaluation of Responses")
-evaluation_entry = data[0]
-prompt = evaluation_entry["prompt"]
-
-# Display the prompt
-prompt_container = st.container()
-prompt_container.text_area(
-    "Prompt",
-    value=prompt,
-    height=150,
-    max_chars=None,
-    key=None,
-    help=None,
-    disabled=True,
-)
-
-
-def selection_made(selected_model):
-    if st.session_state.button_lock:
-        return
-
-    st.session_state.button_lock = True  # Lock the buttons
-    selection_info = {
-        "model1": model1["model"],
-        "model2": model2["model"],
-        "selected": selected_model
-        if selected_model == "tie"
-        else selected_model["model"],
-        "response1": response1,
-        "response2": response2,
-        "prompt": prompt,
-        "time_spent": round(time.time() - st.session_state.start_time, 2),
-        "task_completed": False,
-    }
-    save_selection(
-        st.session_state.worker_id, selection_info, st.session_state.start_time
+# Display completion message and code
+def display_completion_message():
+    completion_code = retrieve_unique_code()
+    mark_task_completed(
+        st.session_state.worker_id, completion_code, st.session_state.worker_count
     )
-    st.session_state.start_time = time.time()
-    next_pair()
-    st.session_state.button_lock = False  # Unlock the buttons
+    st.success("Your evaluations have been submitted. Thank you!")
+    st.write(f"Your completion code is: {completion_code}")
 
 
-# MTurk Worker ID input
-if not st.session_state.worker_id:
-    worker_id = prompt_container.text_input(
-        "Please enter your MTurk Worker ID to start the task.", key="input_worker_id"
-    )
-    if worker_id and has_completed(worker_id):
-        st.error("This Worker ID has already completed the task.")
-    elif worker_id:
-        # Only proceed if the worker hasn't completed the task
-        st.session_state.worker_id = worker_id
-        st.session_state.start_time = time.time()
-        st.session_state.models = evaluation_entry["responses"]
-        st.session_state.combinations = list(
-            combinations(range(len(st.session_state.models)), 2)
-        )
-        random.shuffle(st.session_state.combinations)  # Shuffle the combinations
-        st.session_state.current_combination_index = 0
-        st.session_state.task_completed = False
-
-
-# Logic for model comparison and evaluation
-if st.session_state.worker_id:
-    if not st.session_state.task_completed:
-        # Display progress bar and model responses only if task is not completed
-        total_combinations = len(st.session_state.combinations)
-        current_combination_index = st.session_state.current_combination_index
-        progress_percentage = (current_combination_index + 1) / total_combinations
-        st.progress(progress_percentage)
-
-        idx1, idx2 = st.session_state.combinations[current_combination_index]
-        model1 = st.session_state.models[idx1]
-        model2 = st.session_state.models[idx2]
-
-        response1 = get_random_response(model1["model_responses"])
-        response2 = get_random_response(model2["model_responses"])
-
-        col1, col2 = st.columns(2)
-
-        with col1:
-            st.subheader("Response 1")
-            st.text_area(
-                "First Model's Response", value=response1, height=150, disabled=True
-            )
-            if st.button("Response 1 is better", key="btn1"):
-                selection_made(model1)
-
-        with col2:
-            st.subheader("Response 2")
-            st.text_area(
-                "Second Model's Response", value=response2, height=150, disabled=True
-            )
-            if st.button("Response 2 is better", key="btn2"):
-                selection_made(model2)
-
-        if st.button("Select this if there is a tie", key="tie"):
-            selection_made("tie")
-    else:
-        # Task completed: display completion code
-        completion_code = generate_completion_code(st.session_state.worker_id)
-        mark_task_completed(st.session_state.worker_id)
-        prompt_container.empty()
-        st.success("The task has been completed.")
-        st.write("Your completion code is:", completion_code)
+# Ensure the main function is called to run the app
+if __name__ == "__main__":
+    main()
